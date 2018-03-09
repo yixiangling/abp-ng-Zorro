65bc4c7b7d5b088b7ec6e7320cc569edb1c34115
易湘陵 <yixiangling@live.com>
2018-03-09 09:45:31 +0800

    替换abp默认message和notify调用成ng-zorro

--- src/root.component.ts
+++ src/root.component.ts
@@ -1,9 +1,25 @@
-﻿import { Component } from '@angular/core';
+﻿import { Injector, Component } from '@angular/core';
+
+import { AbpMessageService } from './shared/helpers/message.service';
+import { AbpNotifyService } from './shared/helpers/notify.service';
 
 @Component({
     selector: 'app-root',
     template: `<router-outlet></router-outlet>`
 })
 export class RootComponent {
+    notify: AbpNotifyService;
+    message: AbpMessageService;
 
+    constructor(injector: Injector){
+        this.notify = injector.get(AbpNotifyService);
+        this.message = injector.get(AbpMessageService);
+
+        this.replaceAbpModule();
+    }
+    
+    replaceAbpModule(): void{
+        abp.message = this.message;
+        abp.notify = this.notify;
+    }
 }
