05f89e6f7b78b8b2876f58036791b5ba1d7af7fb
易湘陵 <yixiangling@live.com>
2018-03-09 20:34:18 +0800

    迁移delon的desc-list组件

--- src/shared/components/delon-component.module.ts
+++ src/shared/components/delon-component.module.ts
@@ -3,16 +3,18 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 import { AdProHeaderModule } from './pro-header/pro-header.module';
 import { AdAvatarListModule } from './avatar-list/avatar-list.module';
 import { AdCountDownModule } from './count-down/count-down.module';
+import { AdDescListModule } from './desc-list/desc-list.module';
 
 const MODULES = [
-    AdProHeaderModule, AdAvatarListModule, AdCountDownModule
+    AdProHeaderModule, AdAvatarListModule, AdCountDownModule, AdDescListModule
 ];
 
 @NgModule({
     imports: [
         AdProHeaderModule.forRoot(), 
         AdAvatarListModule.forRoot(),
-        AdCountDownModule.forRoot()
+        AdCountDownModule.forRoot(),
+        AdDescListModule.forRoot()
     ],
     exports: MODULES
 })
