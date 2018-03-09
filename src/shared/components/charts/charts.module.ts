a458110e64a67fc3c9b2b86ca05f5a1607d59ef1
易湘陵 <yixiangling@live.com>
2018-03-10 07:58:26 +0800

    迁移delon的charts所有组件

--- src/shared/components/charts/charts.module.ts
+++ src/shared/components/charts/charts.module.ts
@@ -1,23 +1,23 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-// import { ChartCardComponent } from './card/card.component';
-// import { MiniAreaComponent } from './mini-area/mini-area.component';
-// import { MiniBarComponent } from './mini-bar/mini-bar.component';
-// import { MiniProgressComponent } from './mini-progress/mini-progress.component';
-// import { G2BarComponent } from './bar/bar.component';
-// import { G2PieComponent } from './pie/pie.component';
-// import { TimelineComponent } from './timeline/timeline.component';
-// import { GaugeComponent } from './gauge/gauge.component';
-// import { TagCloudComponent } from './tag-cloud/tag-cloud.component';
-// import { WaterWaveComponent } from './water-wave/water-wave.component';
-// import { G2RadarComponent } from './radar/radar.component';
-// import { ChartComponent } from './chart/chart.component';
+import { ChartCardComponent } from './card/card.component';
+import { MiniAreaComponent } from './mini-area/mini-area.component';
+import { MiniBarComponent } from './mini-bar/mini-bar.component';
+import { MiniProgressComponent } from './mini-progress/mini-progress.component';
+import { G2BarComponent } from './bar/bar.component';
+import { G2PieComponent } from './pie/pie.component';
+import { TimelineComponent } from './timeline/timeline.component';
+import { GaugeComponent } from './gauge/gauge.component';
+import { TagCloudComponent } from './tag-cloud/tag-cloud.component';
+import { WaterWaveComponent } from './water-wave/water-wave.component';
+import { G2RadarComponent } from './radar/radar.component';
+import { ChartComponent } from './chart/chart.component';
 
 const COMPONENTS = [
-    // ChartCardComponent, MiniAreaComponent, MiniBarComponent, MiniProgressComponent,
-    // G2BarComponent, G2PieComponent, TimelineComponent, GaugeComponent, TagCloudComponent,
-    // WaterWaveComponent, G2RadarComponent, ChartComponent
+    ChartCardComponent, MiniAreaComponent, MiniBarComponent, MiniProgressComponent,
+    G2BarComponent, G2PieComponent, TimelineComponent, GaugeComponent, TagCloudComponent,
+    WaterWaveComponent, G2RadarComponent, ChartComponent
 ];
 
 // region: zorro modules
